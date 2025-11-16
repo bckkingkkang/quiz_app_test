05fb9503270f9fe3142d900a29cf9b5618e7340a
가현 <bufar1014@gmail.com>
2025-11-16 22:40:25 +0900

    Initial commit

---
+++ lib/screen/screen_home.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_test/model/model_quiz.dart';
+import 'package:quiz_app_test/screen/screen_quiz.dart';
+
+class HomeScreen extends StatefulWidget{
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+
+  List<Quiz> quizs = [
+    Quiz.fromMap({
+      'title' : 'test',
+      'candidates' : ['a','b','c','d'],
+      'answer' : 0,
+    }),
+    Quiz.fromMap({
+      'title' : 'test',
+      'candidates' : ['a','b','c','d'],
+      'answer' : 0,
+    }),
+    Quiz.fromMap({
+      'title' : 'test',
+      'candidates' : ['a','b','c','d'],
+      'answer' : 0,
+    })
+  ];
+  
+
+  @override
+  Widget build (BuildContext context){
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height; 
+
+    return SafeArea(
+      child : Scaffold (
+        appBar : AppBar(
+          title: Text('My Quiz App'),
+          backgroundColor : Colors.deepPurple,
+          leading : Container(),
+        ),
+        body : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children : <Widget> [
+            Center (
+              child: Image.asset(
+                'images/quiz.jpeg', 
+                width : width*0.8,
+                ),
+              ),
+            Padding(padding:EdgeInsets.all(width*0.024),
+            ),
+            Text(
+              'flutter quiz app',
+              style : TextStyle(
+                fontSize : width*0.065,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'notice\n읽은 후 퀴즈 풀기',
+              textAlign : TextAlign.center,
+            ),
+            Padding(
+              padding:EdgeInsets.all(width*0.048),
+            ),
+
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요'),
+            _buildStep(width, '2. 문제를 읽고 정답을 고른 뒤\n다음 문제 버튼을 누르세요'),
+            _buildStep(width, '3. 3문제를 모두 풀면 결과가 나옵니다'),
+
+            Padding(
+              padding: EdgeInsets.all(width*0.048),
+            ),
+            Container(
+              padding : EdgeInsets.only(bottom:width*0.036),
+              child : Center(
+                child:ButtonTheme(
+                  minWidth:width*0.08, 
+                  height: height*0.05,
+                  shape:RoundedRectangleBorder(
+                    borderRadius:BorderRadius.circular(10),
+                  ),
+                  child:ElevatedButton(
+                    style:ElevatedButton.styleFrom(
+                      backgroundColor:Colors.deepPurple,
+                      minimumSize:Size(width*0.8, height*0.05),
+                      shape : RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: (){
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(
+                          builder : (context) => QuizScreen(
+                            quizs : quizs,
+                          )
+                      ));
+                    },
+                    child:Text (
+                      'Start Quiz',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ),
+                )
+              )
+            ),
+
+          ],
+        )
+      )
+    );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding : EdgeInsets.fromLTRB(
+        width *0.048,
+        width *0.024,
+        width *0.048,
+        width *0.024,    
+      ),
+      child : Row(crossAxisAlignment: CrossAxisAlignment.start,
+      children : <Widget>[
+        Icon(
+          Icons.check_box,
+          size : width*0.04,
+        ),
+        Padding(
+          padding:EdgeInsets.only(right:width*0.024),
+        ),
+        Text(
+          title 
+        ),
+      ])
+    );  
+    
+  }
+
+
+}
